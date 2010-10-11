da20487dcf3b079dd7e29d6a3152e6c11abc516a
Mike Richardson <mike@begrand.net>
2010-10-11 11:36:36 +0100

    Rik's final jQuery tweaks for UAT

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
 
 	$("a[href^='/evince/dir/atoz/']").addClass("atoz-link"); 
+	$("a[href^='mailto']").addClass("mail-link");
+	
+	$('#main-content a').filter(function() {
+         return this.hostname && this.hostname !== location.hostname;
+        }).addClass('external-link');
  	
-
+ 	$("ul#stockton-cats  a").tipTip();
+ 	$("ul.pager  a").tipTip();
+ 	
+ 		$(" img").lazyload({ 
+    		effect : "fadeIn"
+});
 
 
  });
+ 
+ 
